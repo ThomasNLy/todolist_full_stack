4f0aef67cd5e9e7b81b64c97e9549b1d0fe26aae
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2024-02-06 19:33:59 -0500

    updated to include a taskid parameter

--- server/Model/ToDoSchema.js
+++ server/Model/ToDoSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
+  taskid: {type: String, required: true},
   title: { type: String, required: true },
   description: String,
   completed: { type: Boolean, default: false },
